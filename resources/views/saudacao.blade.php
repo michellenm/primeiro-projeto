0685f05cb686eb9b619dae76b4f0ed1085fdd943
php 24 <php.24@daphne.cec.ime.usp.br>
2018-02-02 22:10:30 -0200

    add saudação

--- resources/views/saudacao.blade.php
+++ resources/views/saudacao.blade.php
@@ -3,11 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>Saudação </title>
+  <link rel="stylesheet" type="text/css" href="/css/app.css">
   </head>
   <body>
-
   @for($i = 0; $i<100; $i++)
-    <h1> Saudação Corintiana {{ $i }}</h1>
+<h1> Saudação Corintianas!!! {{ $i }}</h1>
   @endfor
+  <script type="text/javascript" src="/js/app.js">
   </body>
 </html>
